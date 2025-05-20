4f1bb882be068273c6300a3d6b417216b33253fd
subha <av6developer@gmail.com>
2025-05-20 19:14:14 +0530

    Minor Bug Fixed (AuthController)

--- src/app/modules/layout/components/navbar/profile-menu/profile-menu.component.ts
+++ src/app/modules/layout/components/navbar/profile-menu/profile-menu.component.ts
@@ -5,6 +5,8 @@ import { ClickOutsideDirective } from '../../../../../shared/directives/click-ou
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { ThemeService } from '../../../../../core/services/theme.service';
 import { trigger, state, style, animate, transition } from '@angular/animations';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { remult, UserInfo } from 'remult';
 
 @Component({
   selector: 'app-profile-menu',
@@ -87,10 +89,13 @@ export class ProfileMenuComponent implements OnInit {
   ];
 
   public themeMode = ['light', 'dark'];
+   profile:  any | undefined;
 
-  constructor(public themeService: ThemeService) {}
+  constructor(public themeService: ThemeService, private authService:AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.profile = remult.user;
+  }
 
   public toggleMenu(): void {
     this.isOpen = !this.isOpen;
@@ -107,5 +112,10 @@ export class ProfileMenuComponent implements OnInit {
     this.themeService.theme.update((theme) => {
       return { ...theme, color: color };
     });
+  }
+
+   logout() {
+    this.isOpen = false;
+    this.authService.logout();
   }
 }
--- src/demo/auth/User_old.ts
+++ src/demo/auth/User_old.ts
@@ -2,7 +2,7 @@ import { Entity, Fields, remult, repo, Validators } from "remult";
 
 // import type { ProviderType } from "../../server/auth.js";
 import { Roles } from "./Roles_old.js";
-import bcrypt from "bcryptjs";
+// import bcrypt from "bcryptjs";
 
 @Entity("users", {
   allowApiCrud: remult.authenticated, // Only authenticated users can perform CRUD operations
